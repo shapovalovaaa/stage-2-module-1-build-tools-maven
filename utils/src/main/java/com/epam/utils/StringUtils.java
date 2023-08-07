282096171829e94e0e1c88bbcd0fc237cc9a41d1
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-07 11:11:11 +0300

    feat: solve task

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,16 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return Optional.ofNullable(str)
+                .filter(NumberUtils::isCreatable)
+                .map(NumberUtils::createBigDecimal)
+                .map(decimal -> decimal.compareTo(BigDecimal.ZERO) > 0)
+                .orElse(false);
     }
 }
